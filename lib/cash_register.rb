77c39d62b4bfd336a91795a8a19d056414084fc6
Will Hodgson <will.hodgson@current-rms.com>
2019-08-05 12:30:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ class CashRegister
 
   def apply_discount
     if @discount > 0
-      @total = @total * (100 - @discount)
+      @total = @total - @discount
     else
       "There is no discount to apply."
     end
