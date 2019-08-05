e79e55feabdad44607e4eac8c5b4de8204a732ac
Will Hodgson <will.hodgson@current-rms.com>
2019-08-05 12:22:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
     @items = []
   end
 
-  def add_item(title, price, quantity=0)
+  def add_item(title, price, quantity=1)
     @quantity = quantity
     @items << title
     @total += price
